732e4985505e8260107d13b0703258f9ea44bda9
Sami  Sabeeh <samsabeeh6@gmail.com>
2020-06-24 06:30:24 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
+
 def oxford_comma(array)
+  if array.length == 1 
+    array.join
+  elsif array.length == 2 
+    array.join(" and ")
+  elsif array.length >= 3
+  new_array_item = "and #{array[-1]}"
+  array.pop
+  array << new_array_item
+  array.join(", ")
+  
+  
+  
+end
 
 end
