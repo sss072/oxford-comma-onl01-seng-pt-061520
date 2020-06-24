fc161cb07972becc52f30769aafe5275e231d47e
Sami  Sabeeh <samsabeeh6@gmail.com>
2020-06-24 06:06:15 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,11 @@
+
 def oxford_comma(array)
   if array.length == 1 
     array.join
   elsif array.length == 2 
     array.join(" and ")
-  elsif array.length == 3
+  elsif array.length >= 3
+  array << " and #{array[-1]}"
   array.join(",")
   array << " and "
 end
